83042813c5e72b2106511fc4cc9397119289e09c
Nguyen Quoc An <117843975+nguyenquocan0101@users.noreply.github.com>
2025-07-24 20:31:39 +0700

    update

--- src/components/Layout/Header/Navigation.jsx
+++ src/components/Layout/Header/Navigation.jsx
@@ -2,12 +2,6 @@ import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./Navigation.css";
 
-const serviceOptions = [
-  { label: "Tư vấn sức khỏe giới tính ", href: "/services?type=tu-van" },
-  { label: "Xét nghiệm STIs", href: "/appointment" },
-  { label: "Đăng kí khám bệnh", href: "/services?type=dangki" },
-];
-
 // Removed blog options - no longer using tag-based navigation
 
 const Navigation = () => {
@@ -84,18 +78,7 @@ const Navigation = () => {
               >
                 {item.label}
               </Link>
-              {/* Dropdown for Dịch vụ */}
-              {item.dropdown && item.label === "Dịch vụ" && (
-                <ul className="dropdown-menu">
-                  {serviceOptions.map((opt, idx) => (
-                    <li key={idx}>
-                      <Link to={opt.href} className="dropdown-link">
-                        {opt.label}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              )}
+
               {/* Removed Tin tức dropdown - now goes directly to /blog */}
             </li>
           );
